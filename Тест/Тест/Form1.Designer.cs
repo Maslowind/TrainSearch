6e604512e9413392071ea541a1480b938bc6d987
Maslowind <45139025+Maslowind@users.noreply.github.com>
2019-01-19 14:14:31 +0200

    в
    Программа для поиска жд билетов.

---
+++ Тест/Тест/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Тест
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 579);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Откуда";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Днепр-Главный";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 195);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Введите данные";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(139, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "год";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(117, 128);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(70, 22);
+            this.textBox6.TabIndex = 12;
+            this.textBox6.Text = "2019";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "месяц";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(61, 128);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(40, 22);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.Text = "01";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "день";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 128);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(40, 22);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "20";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Когда";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Куда";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(139, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Трускавец";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(272, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 20;
+            this.dataGridView1.RowTemplate.Height = 60;
+            this.dataGridView1.Size = new System.Drawing.Size(927, 590);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 614);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Поиск жд Билетов по Украине";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Тест/Тест/Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+using System.Xml;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
+using System.Web;
+
+namespace Тест
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void TakeXMLtickets(string from, string to, string date, string time)
+        {
+            using (var wb = new WebClient())
+            {
+                var data = new NameValueCollection();
+                data["from"] = from;
+                data["to"] = to;
+                data["date"] = date;
+                data["time"] = time;
+                var response = wb.UploadValues(@"https://booking.uz.gov.ua/ru/train_search/", "POST", data);
+                string responseText = Encoding.UTF8.GetString(response);
+                XNode node = JsonConvert.DeserializeXNode(responseText, "Root");
+                System.IO.File.WriteAllText(TakePath()+@"\КодBooking.xml", Convert.ToString(node));
+            }
+        }
+        private void TakeStationList(string from, string to, string date, string time)
+        {
+            using (var wb = new WebClient())
+            {
+                var data = new NameValueCollection();
+                data["routes[0][from]"] = from;
+                data["routes[0][to]"] = to;
+                data["routes[0][date]"] = date;
+                data["routes[0][train]"] = time;
+                var response = wb.UploadValues(@"https://booking.uz.gov.ua/ru/route/", "POST", data);
+                string responseText = Encoding.UTF8.GetString(response);
+                XNode node = JsonConvert.DeserializeXNode(responseText, "Root");
+                System.IO.File.WriteAllText(TakePath() + @"\КодBooking3.xml", Convert.ToString(node));
+            }
+        }
+        private string TakeStationNumber(string StationName)
+        {
+            string HttpAd = Uri.EscapeDataString(StationName);
+            string HtmlText = string.Empty;
+            HttpWebRequest myHttwebrequest = (HttpWebRequest)HttpWebRequest.Create(@"https://booking.uz.gov.ua/ru/train_search/station/?term="+HttpAd);
+            HttpWebResponse myHttpWebresponse = (HttpWebResponse)myHttwebrequest.GetResponse();
+            StreamReader strm = new StreamReader(myHttpWebresponse.GetResponseStream());
+            HtmlText = strm.ReadToEnd();
+            bool b = false;
+            XmlNode xRoot = JsonConvert.DeserializeXmlNode("{\"data\":{\"list\":" + HtmlText + "}}");
+            foreach (XmlNode xRoot1 in xRoot)
+            {
+                foreach (XmlNode xnode in xRoot1.ChildNodes)
+                {
+                    foreach (XmlNode childnode in xnode.ChildNodes)
+                    {
+                        if (childnode.Name == "title" && childnode.InnerText==StationName)
+                        {
+                            b = true;
+                        }
+                        if (childnode.Name == "value" &&b)
+                        {
+                            return childnode.InnerText;
+                        }
+                        
+
+                    } 
+                }
+            }
+            return null;
+        }
+        private string TakePath()
+        {
+            string executableLocation = Path.GetDirectoryName(
+            Assembly.GetExecutingAssembly().Location);
+            return Path.Combine(executableLocation);
+        }
+        private string[,] TakeInformation(ref int i)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(TakePath() + @"\КодBooking.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            int arrayl = 2;
+            string[,] table = new string[arrayl, 6];
+           
+            foreach (XmlNode xRoot1 in xRoot)
+            {
+                foreach (XmlNode xnode in xRoot1.ChildNodes)
+                {
+                   foreach (XmlNode childnode in xnode.ChildNodes)
+                    {
+                        if (childnode.Name == "num")
+                        {
+                            table[i, 0] = childnode.InnerText;                   
+                        }
+                        if (childnode.Name == "travelTime")
+                        {
+                            table[i, 4] = childnode.InnerText;
+                        }
+                        if (childnode.Name == "types")
+                        {
+                            foreach (XmlNode childnode2 in childnode.ChildNodes)
+                            {
+                                if (childnode2.Name == "id")
+                                {
+                                    table[i, 5] += (childnode2.InnerText + ":");
+                                }
+                                if (childnode2.Name == "places")
+                                {
+                                    table[i, 5] += (childnode2.InnerText + "; ");
+                                }
+                            }
+                        }
+                        if (childnode.Name == "from")
+                        {
+                            string g="";
+                            foreach (XmlNode childnode2 in childnode.ChildNodes)
+                            {
+                                
+                                if (childnode2.Name == "stationTrain")
+                                {
+                                    table[i, 1] = (childnode2.InnerText);
+                                }
+                                if (childnode2.Name == "time")
+                                {
+                                    table[i, 2] =g.Insert(0,childnode2.InnerText + " ");
+                                }
+                                if (childnode2.Name == "date")
+                                {
+                                    g = (childnode2.InnerText);
+                                }
+                            }
+                        }
+                        if (childnode.Name == "to")
+                        {
+                            string g = "";
+                            foreach (XmlNode childnode2 in childnode.ChildNodes)
+                            {
+
+                                if (childnode2.Name == "stationTrain")
+                                {
+                                    table[i,1] +=" - "+ (childnode2.InnerText);
+                                }
+                                if (childnode2.Name == "time")
+                                {
+                                    table[i, 3] = g.Insert(0, childnode2.InnerText + " ");
+                                }
+                                if (childnode2.Name == "date")
+                                {
+                                    g = (childnode2.InnerText);
+                                }
+                            }
+                        }
+                    }
+                  if(xnode.Name!="warning")
+                  {
+                      i++;
+                    table = (string[,])ResizeArray(table, new int[] { arrayl++, 6 }); }
+                    
+                } 
+            }
+            return table;
+        }
+        private string[,] TakeTrainStations(ref int i)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(TakePath() + @"\КодBooking3.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            int arrayl = 2;
+            string[,] table = new string[arrayl, 2];
+
+            foreach (XmlNode xRoot2 in xRoot)
+            {
+                foreach (XmlNode xRoot1 in xRoot2)
+            {
+                     
+                            foreach (XmlNode xnode in xRoot1.ChildNodes)
+                            {
+                                if (xnode.Name != "tpl")
+                                {
+                                    foreach (XmlNode childnode in xnode.ChildNodes)
+                                    {
+                                        if (childnode.Name == "name")
+                                        {
+                                            table[i, 0] = childnode.InnerText;
+                                        }
+                                        if (childnode.Name == "departureTime")
+                                        {
+                                            table[i, 1] = childnode.InnerText;
+                                        }
+
+                                    }
+                                    if (xnode.Name != "train" && xnode.Name != "#text")
+                                    {
+                                        i++;
+                                        table = (string[,])ResizeArray(table, new int[] { arrayl++, 2 });
+                                    }
+                                }
+                           
+                }
+                }
+            }
+            return table;
+        }
+            private static Array ResizeArray(Array arr, int[] newSizes)
+   {
+      if (newSizes.Length != arr.Rank)
+         throw new ArgumentException("arr must have the same number of dimensions " +
+                                     "as there are elements in newSizes", "newSizes"); 
+
+      var temp = Array.CreateInstance(arr.GetType().GetElementType(), newSizes);
+      int length = arr.Length <= temp.Length ? arr.Length : temp.Length;
+      Array.ConstrainedCopy(arr, 0, temp, 0, length);
+      return temp;
+   }
+
+            private int AddToTable()
+            {
+                int i = 0;
+                string[,] table = TakeInformation(ref i);
+                int m = 0;
+                
+                for (int j = 0; j < i; j++)
+                {
+                    for (int k = 0; k < 6; k++)
+                        { 
+                            dataGridView1[k, j].Value = "";
+                        } dataGridView1.RowCount++;
+                    
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    if (table[j, 5] != null)
+                    {
+                        for (int k = 0; k < 6; k++)
+                        {
+                            dataGridView1[k, j-m].Value = table[j, k];
+                        } //dataGridView1.RowCount++;
+                        
+                    }else m++;
+                }
+                return m;
+            }
+            private int AddToTable2(int n,double sec1)
+            {
+                int i = 0;
+                string[,] table = TakeInformation(ref i);
+                int m = 0; dataGridView1.RowCount++;
+                
+                for (int j = 0; j < i; j++)
+                {
+                    if (table[j, 5] != null)
+                    {
+                        string[] mas = table[j, 3].Split(' ');
+                        double sec2 = TimeSpan.Parse(mas[0]).TotalSeconds;
+                        DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                        for (int k = 0; k < 6; k++)
+                        {
+                            
+                            row.Cells[k].Value =table[j, k];
+                            
+                        }
+                        if (sec2 >= sec1)
+                        {
+                            this.dataGridView1.Rows.Add(row);
+                        }
+
+
+                    }
+                    else m++;
+                }
+                return  m;
+            }            
+                    
+                
+            
+        public void toTable()
+        {
+            
+                dataGridView1.ColumnCount = 6;
+                dataGridView1.RowCount = 2;
+                dataGridView1.Columns[0].HeaderText = "Номер";
+                dataGridView1.Columns[1].HeaderText = "Откуда/Куда";
+                dataGridView1.Columns[2].HeaderText = "Время отправления";
+                dataGridView1.Columns[3].HeaderText = "Время прибытия";
+                dataGridView1.Columns[4].HeaderText = "В пути";
+                dataGridView1.Columns[5].HeaderText = "Тип мест";
+                this.dataGridView1.DefaultCellStyle.WrapMode =DataGridViewTriState.True;
+                dataGridView1.RowTemplate.MinimumHeight = 60;
+                DataGridViewColumn column = dataGridView1.Columns[1];
+                column.Width = 120;
+           
+        }
+        public int ChooseTrain()
+        {
+            int i = 0, j = 0;
+            int MaxValue = -2147483647, MaxK=0;
+            string[,] train = TakeInformation(ref i); 
+            for(int k=0; k<i; k++)
+            {
+                TakeStationList(TakeStationNumber(Convert.ToString(textBox2.Text)),
+         TakeStationNumber(Convert.ToString(textBox3.Text)), Convert.ToString(textBox6.Text) +
+     "-" + Convert.ToString(textBox5.Text) + "-" +
+     Convert.ToString(textBox4.Text),
+     train[k, 0]);
+                j = 0;
+                string[,] trainStation = TakeTrainStations(ref j);
+                if (MaxValue < j) { MaxValue = j; MaxK = k; }
+            }
+            return MaxK;
+            
+        }
+
+        private string ConvertDate(string OldDate)
+        {
+            string[] mas = OldDate.Split('.');
+            return mas[2]+"-"+mas[1]+"-"+mas[0];
+        }
+       private void AlternateTicket(string[,]train)
+        {
+            int k = 0;
+            string[,] trainStation = TakeTrainStations(ref k);
+            
+                int t = ChooseTrain();
+                TakeStationList(TakeStationNumber(Convert.ToString(textBox2.Text)),
+                     TakeStationNumber(Convert.ToString(textBox3.Text)), Convert.ToString(textBox6.Text) +
+                 "-" + Convert.ToString(textBox5.Text) + "-" +
+                 Convert.ToString(textBox4.Text),
+                 train[t, 0]);
+                
+                for (int j = 1; j < k; j++)
+                {int i = 0;
+                   string[,] train2 =  TakeInfAboutConnection(TakeStationNumber(Convert.ToString(textBox2.Text)),
+                     TakeStationNumber(trainStation[k-1-j,0]),
+                     Convert.ToString(textBox6.Text) + "-" + Convert.ToString(textBox5.Text) + "-" + Convert.ToString(textBox4.Text),
+                     ref i);
+                     string[] mas = train[t, 3].Split(' ');
+                     string NewDate = ConvertDate(mas[2]);
+                     double sec1 = TimeSpan.Parse(mas[0]).TotalSeconds;
+
+                    toTable();
+                    int m = AddToTable();
+                    if(m!=i)
+                    {
+                        int l= 0;
+                        string[,] train3 = TakeInfAboutConnection(TakeStationNumber(trainStation[k - 1 - j, 0]),
+                          TakeStationNumber(Convert.ToString(textBox3.Text)),
+                          //Convert.ToString(textBox6.Text) + "-" + Convert.ToString(textBox5.Text) + "-" + Convert.ToString(textBox4.Text),
+                          NewDate,
+                          ref l);
+                        label7.Text="На станции "+ trainStation[k - 1 - j, 0]+ " у Вас пересадка!";
+                        toTable();
+                         int n=AddToTable2(m,sec1);
+                        if(l!=n)
+                        {
+                            break;
+                        }
+                    }
+                }           
+        }
+        private string [,] TakeInfAboutConnection(string s1,string s2, string date, ref int i)
+       {
+           TakeXMLtickets(s1,s2,date,"00:00");
+           return TakeInformation(ref i);
+       }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            string [,] train=TakeInfAboutConnection(TakeStationNumber(Convert.ToString(textBox2.Text)),
+                     TakeStationNumber(Convert.ToString(textBox3.Text)),
+                     Convert.ToString(textBox6.Text) + "-" + Convert.ToString(textBox5.Text) + "-" + Convert.ToString(textBox4.Text),
+                     ref i);
+            
+            
+            toTable();
+            int m=AddToTable();            
+            if (m == i)
+            {
+            AlternateTicket(train);
+            }
+        }
+        }
+
+       
+}
+            
+            
+        
+    
+
